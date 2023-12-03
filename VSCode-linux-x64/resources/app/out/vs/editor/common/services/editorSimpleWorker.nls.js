2181a64d5f4164aef8fa50dd8935f9d4d180800d
daniel12334j <daniel-juininho@outlook.com>
2023-12-03 11:49:01 -0300

    commit inicial

---
+++ VSCode-linux-x64/resources/app/out/vs/editor/common/services/editorSimpleWorker.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/define("vs/editor/common/services/editorSimpleWorker.nls",{"vs/base/common/platform":["_"],"vs/editor/common/languages":["array","boolean","class","constant","constructor","enumeration","enumeration member","event","field","file","function","interface","key","method","module","namespace","null","number","object","operator","package","property","string","struct","type parameter","variable","{0} ({1})"]});
+
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/1a5daa3a0231a0fbba4f14db7ec463cf99d7768e/core/vs/editor/common/services/editorSimpleWorker.nls.js.map

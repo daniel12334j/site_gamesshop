ec3c31c41a3634296e273f6a3b52c9f4d7c2aa93
daniel12334j <138599908+daniel12334j@users.noreply.github.com>
2023-12-03 11:58:54 -0300

    Delete VSCode-linux-x64 directory

--- VSCode-linux-x64/resources/app/out/vs/base/common/worker/simpleWorker.nls.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls",{"vs/base/common/platform":["_"]});
-
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/1a5daa3a0231a0fbba4f14db7ec463cf99d7768e/core/vs/base/common/worker/simpleWorker.nls.js.map
--- VSCode-linux-x64/resources/app/out/vs/workbench/contrib/debug/node/telemetryApp.nls.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/define("vs/workbench/contrib/debug/node/telemetryApp.nls",{"vs/base/common/platform":["_"]});
-
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/1a5daa3a0231a0fbba4f14db7ec463cf99d7768e/core/vs/workbench/contrib/debug/node/telemetryApp.nls.js.map
